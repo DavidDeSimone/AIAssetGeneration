a7b26036aed4ea460cd4d759ca2cef1d50910283
David DeSimone <lord.good.mail@gmail.com>
2023-03-22 17:40:45 -0700

    Initial commit for repo

---
+++ Packages/com.recursiverhapsody.aiassetgenerator/Editor/Generator.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace com.recursiverhapsody
+{
+    public class Generator : ScriptableObjectWithGuid
+    {
+
+
+
+
+
+
+
+    }
+}
