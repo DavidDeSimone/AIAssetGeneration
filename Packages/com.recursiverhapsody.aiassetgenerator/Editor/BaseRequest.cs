7bcda184de4936449d6093637fc21a252922aaf2
David DeSimone <lord.good.mail@gmail.com>
2023-03-22 17:40:45 -0700

    It's alive - ChatGPT is able to talk via HTTP

--- Packages/com.recursiverhapsody.aiassetgenerator/Editor/BaseRequest.cs
+++ Packages/com.recursiverhapsody.aiassetgenerator/Editor/BaseRequest.cs
@@ -1,18 +1,30 @@
 using UnityEngine;
 using UnityEditor;
 using System.Threading.Tasks;
+using System.Threading;
+using UnityEngine.Networking;
+using System.Collections.Generic;
+using System;
+using System.Collections;
+using System.Text;
 
 namespace com.recursiverhapsody
 {
-    public enum ChatModels
+    public class ChatModel
     {
-        ChatGPT_3_5,
-        Davinci_3,
+        public const string ChatGPT_3_5 = "gpt-3.5-turbo";
+        public const string Davinci_3 = "text-davinci-003";
     }
 
+    public class Roles
+    {
+        public const string User = "user";
+        public const string System = "system";
+        public const string Assistant = "assistant";
+    }
 
     public interface IOpenAIRequest {
-        public Task Request();
+        public IEnumerator Request();
     }
     public abstract class BaseOpenAIRequest : IOpenAIRequest
     {
@@ -20,18 +32,126 @@ namespace com.recursiverhapsody
         public BaseOpenAIRequest(string apiKey)
         {
             this.apiKey = apiKey;
-            Debug.Log("Created with APIKEY " + apiKey);
         }
 
+        protected virtual void setContentHeaders(UnityWebRequest unityWebRequest)
+        {
+            unityWebRequest.SetRequestHeader("Content-Type", "application/json");
+            unityWebRequest.SetRequestHeader("Authorization", $"Bearer {apiKey}");
+        }
 
-        public async Task Request()
+        protected abstract UnityWebRequest getWebRequest();
+
+        public static void StartBackgroundTask(IEnumerator update, Action end = null)
+        {
+            EditorApplication.CallbackFunction closureCallback = null;
+
+            closureCallback = () =>
+            {
+                try
+                {
+                    if (update.MoveNext() == false)
+                    {
+                        if (end != null)
+                        {
+                            end();
+                        }
+
+                        EditorApplication.update -= closureCallback;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    if (end != null)
+                    {
+                        end();
+                    }
+
+                    Debug.LogException(ex);
+                    EditorApplication.update -= closureCallback;
+                }
+            };
+
+            EditorApplication.update += closureCallback;
+        }
+
+        public void SendRequest()
+        {
+            StartBackgroundTask(Request());
+        }
+
+        public IEnumerator Request()
         {
             Debug.Log("Requesting.....");
-        } 
+
+            using (var webRequest = getWebRequest()) {
+                setContentHeaders(webRequest);
+                webRequest.downloadHandler = (DownloadHandler) new DownloadHandlerBuffer();
+                webRequest.disposeUploadHandlerOnDispose = true;
+                webRequest.disposeDownloadHandlerOnDispose = true;
+
+                Debug.Log("Sending Request...");
+                yield return webRequest.SendWebRequest();
+
+                while (webRequest.isDone == false)
+                {
+                    yield return null;
+                }
+
+                // EditorUtility.ClearProgressBar();
+
+                switch (webRequest.result)
+                {
+                    case UnityWebRequest.Result.ConnectionError:
+                    case UnityWebRequest.Result.DataProcessingError:
+                        Debug.LogError("Error: " + webRequest.error);
+                        break;
+                    case UnityWebRequest.Result.ProtocolError:
+                        Debug.LogError("HTTP Error: " + webRequest.error);
+                        break;
+                    case UnityWebRequest.Result.Success:
+                        Debug.Log(webRequest.downloadHandler.text);
+                        // var responseJson = JsonUtility.FromJson<T>(webRequest.downloadHandler.text);
+                        // callback(responseJson);
+                        break;
+                }
+            }
+
+        }
     }
 
     public class ChatOpenAIRequest : BaseOpenAIRequest 
     {
-        public ChatOpenAIRequest(string apiKey) : base(apiKey) {}
+        [Serializable]
+        public class Message
+        {
+            public string role;
+            public string content;
+        }
+        // @TODO make this a builder pattern
+        [Serializable]
+        public class Parameters
+        {
+            public string model;
+            public List<Message> messages;
+        }
+
+        protected ChatOpenAIRequest.Parameters parameters;
+
+        public ChatOpenAIRequest(string apiKey, ChatOpenAIRequest.Parameters parameters) : base(apiKey) 
+        {
+            this.parameters = parameters;
+        }
+
+        protected override UnityWebRequest getWebRequest()
+        {
+            var json = JsonUtility.ToJson(parameters);
+            Debug.Log(json);
+            var request = new UnityWebRequest("https://api.openai.com/v1/chat/completions", "POST");
+            var bodyRaw = Encoding.UTF8.GetBytes(json);
+            request.uploadHandler = (UploadHandler) new UploadHandlerRaw(bodyRaw);
+
+            return request;
+        }
     }
 }
--- Packages/com.recursiverhapsody.aiassetgenerator/Editor/GlobalGeneratorSettings_Inspector.cs
+++ Packages/com.recursiverhapsody.aiassetgenerator/Editor/GlobalGeneratorSettings_Inspector.cs
@@ -2,6 +2,7 @@ using UnityEditor;
 using UnityEditor.UIElements;
 using UnityEngine.UIElements;
 using UnityEngine;
+using System.Collections.Generic;
 
 
 namespace com.recursiverhapsody
@@ -28,11 +29,22 @@ namespace com.recursiverhapsody
 
         public void OnGenerateClicked()
         {
-            Debug.Log("On Click..." + serializedObject.FindProperty("Prompt").stringValue);
+            var prompt = serializedObject.FindProperty("Prompt").stringValue;
+            Debug.Log("On Click..." + prompt);
             var apiKeyAsset = serializedObject.FindProperty("APIKeyPath").objectReferenceValue as TextAsset;
 
-            var request = new ChatOpenAIRequest(apiKeyAsset.text);
-            request.Request().Wait();
+            var request = new ChatOpenAIRequest(apiKeyAsset.text, new ChatOpenAIRequest.Parameters () {
+                model = ChatModel.ChatGPT_3_5,
+                messages = new List<ChatOpenAIRequest.Message>() {
+                    new ChatOpenAIRequest.Message() {
+                        role = Roles.User,
+                        content = prompt,
+                    }
+                }
+            });
+            // EditorUtility.DisplayProgressBar("Simple Progress Bar", "Doing some work...", 0.5f);
+            request.SendRequest();
+            // EditorUtility.ClearProgressBar();
         }
     }
 
